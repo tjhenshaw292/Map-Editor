e1385d50cdf4307e5aa5ff6b600cf55ee1f3dce7
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-05-16 10:45:54 -0500

    fixed intro map

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -8,7 +8,7 @@ int main()
 	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 14, 22);
 	makeMe.setBlankTile(551);
 	makeMe.setMapFileName("intro.dat");
-	//makeMe.load("intro.dat");
+	makeMe.load("intro.dat");
 	makeMe.display();
 	makeMe.save("intro.dat");
 	return 0;
