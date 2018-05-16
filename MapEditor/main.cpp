ec3e3a710c79c969df0facc3bd7bdf10e6faf572
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-05-16 11:13:35 -0500

    lengthened intro map

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,10 +5,10 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 14, 22);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 14, 23);
 	makeMe.setBlankTile(551);
 	makeMe.setMapFileName("intro.dat");
-	makeMe.load("intro.dat");
+	//makeMe.load("intro.dat");
 	makeMe.display();
 	makeMe.save("intro.dat");
 	return 0;
