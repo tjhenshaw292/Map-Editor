c39a4a5ddb38db74013c16972f96a97bedd76351
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-05-17 18:05:12 -0500

    working on route 01

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,11 +5,11 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 14, 23);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 19, 69);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("intro.dat");
-	//makeMe.load("intro.dat");
+	makeMe.setMapFileName("route01.dat");
+	makeMe.load("route01.dat");
 	makeMe.display();
-	makeMe.save("intro.dat");
+	makeMe.save("route01.dat");
 	return 0;
 }
