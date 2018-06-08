30ed8119d24ec2a4c9f36d5baacdae93307568bb
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-06-08 16:09:03 -0500

    roadhouse

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -8,7 +8,7 @@ int main()
 	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 22, 16);
 	makeMe.setBlankTile(551);
 	makeMe.setMapFileName("burtHouseInside.dat");
-	//makeMe.load("burtHouse.dat");
+	makeMe.load("burtHouseInside.dat");
 	makeMe.display();
 	makeMe.save("burtHouseInside.dat");
 	return 0;
