2605a6136c2731057f3dd9b567a094e98f756958
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-07-20 18:00:08 -0500

    route 1 added

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,11 +5,11 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 19, 69);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 40, 22);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("burtHouse.dat");
-	makeMe.load("burtHouse.dat");
+	makeMe.setMapFileName("route01.dat");
+	makeMe.load("route01.dat");
 	makeMe.display();
-	makeMe.save("burtHouse.dat");
+	makeMe.save("route01.dat");
 	return 0;
 }
