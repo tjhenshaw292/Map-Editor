d443ea8384aa6d9cfa7b80a06ee95e6bcd7008bf
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-10-17 16:43:43 -0500

    burtHouse edit

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,14 +5,16 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 42, 22);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 20, 69);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("route01.dat");
-	makeMe.load("route01.dat");
+	makeMe.setMapFileName("burtHouseT.dat");
+	makeMe.load("burtHouseT.dat");
 
+	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addColumnToLeft(1);
+	//makeMe.addColumnToRight(1);
 
 	makeMe.display();
-	makeMe.save("route01.dat");
+	makeMe.save("burtHouseT.dat");
 	return 0;
 }
