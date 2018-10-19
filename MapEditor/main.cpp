7f903b2a3ebfbfc21948f8237370e944cf837e87
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-10-19 16:26:23 -0500

    edit to bur hous

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -7,14 +7,14 @@ int main()
 		throw;
 	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 20, 69);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("burtHouseT.dat");
-	makeMe.load("burtHouseT.dat");
+	makeMe.setMapFileName("burtHouse.dat");
+	makeMe.load("burtHouse.dat");
 
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addColumnToLeft(1);
 	//makeMe.addColumnToRight(1);
 
 	makeMe.display();
-	makeMe.save("burtHouseT.dat");
+	makeMe.save("burtHouse.dat");
 	return 0;
 }
