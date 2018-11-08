61cce9da585f8853b632ee32df191de7291b31e2
TJ Henshaw <tjhenshaw292@hotmail.com>
2018-11-08 16:19:03 -0600

    edited town1

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,16 +5,16 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 14, 6);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 44, 42);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("wideHouse.dat");
-	//makeMe.load("wideHouse.dat");
+	makeMe.setMapFileName("town1.dat");
+	makeMe.load("town1.dat");
 
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addColumnToLeft(1);
 	//makeMe.addColumnToRight(1);
 
 	makeMe.display();
-	makeMe.save("wideHouse.dat");
+	makeMe.save("town1.dat");
 	return 0;
 }
