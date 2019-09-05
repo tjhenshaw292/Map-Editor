3df9076b509cef431b1a903955b9fec90d6f085d
TJ Henshaw <tjhenshaw292@hotmail.com>
2019-09-05 17:22:49 -0500

    adding town 1 houses

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,16 +5,16 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 44, 42);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 18, 18);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("town1.dat");
-	makeMe.load("town1.dat");
+	makeMe.setMapFileName("biggerHouse01.dat");
+	makeMe.load("biggerHouse01.dat");
 
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addColumnToLeft(1);
 	//makeMe.addColumnToRight(1);
 
 	makeMe.display();
-	//makeMe.save("b.dat");
+	makeMe.save("biggerHouse01.dat");
 	return 0;
 }
