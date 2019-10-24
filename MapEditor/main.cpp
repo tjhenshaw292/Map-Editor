533d534641fa5d5cb96fa76dcd9f81aae8bc0282
TJ Henshaw <tjhenshaw292@hotmail.com>
2019-10-24 17:09:10 -0500

    phallicHousework

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,16 +5,16 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 18, 18);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 22, 25);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("biggerHouse01.dat");
-	makeMe.load("biggerHouse01.dat");
+	makeMe.setMapFileName("phallicHouse01.dat");
+	makeMe.load("phallicHouse01.dat");
 
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addColumnToLeft(1);
 	//makeMe.addColumnToRight(1);
 
 	makeMe.display();
-	makeMe.save("biggerHouse01.dat");
+	//makeMe.save("biggerHouse01.dat");
 	return 0;
 }
