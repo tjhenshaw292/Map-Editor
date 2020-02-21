8ec275656d37c41088dd132a89acbe43798b2fcb
TJ Henshaw <tjhenshaw292@hotmail.com>
2020-02-21 17:48:12 -0600

    Added Route 02

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,16 +5,19 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 22, 25);
+	MapMaker makeMe("pokeTiles2.png", sf::Vector2u(16, 16), 31, 28);
 	makeMe.setBlankTile(551);
-	makeMe.setMapFileName("phallicHouse01.dat");
-	makeMe.load("phallicHouse01.dat");
+
+	std::string fileName{ "route02.dat" };
+
+	makeMe.setMapFileName(fileName);
+	makeMe.load(fileName);
 
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addColumnToLeft(1);
 	//makeMe.addColumnToRight(1);
 
 	makeMe.display();
-	//makeMe.save("biggerHouse01.dat");
+	makeMe.save(fileName);
 	return 0;
 }
