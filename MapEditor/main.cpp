01e4b430dbef4a668c8b5c4ef8eb59fd44c27543
TJ Henshaw <tjhenshaw292@hotmail.com>
2020-12-15 17:08:01 -0600

    added bill house

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,15 +5,16 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("gen3Outdoors.png", sf::Vector2u(16, 16), 20, 45);
+	MapMaker makeMe("gen3Indoors.png", sf::Vector2u(16, 16), 18, 19);
 	makeMe.setBlankTile(0);
 
-	std::string fileName{ "goldenBridge.dat" };
+	std::string fileName{ "billHouse.dat" };
 
 	makeMe.setMapFileName(fileName);
 	makeMe.load(fileName);
 
 	//In order to use this, just add the row/col then reOpen mapmaker
+	//makeMe.addRowToTop(1);
 	//makeMe.addColumnToLeft(1);
 	//makeMe.addColumnToRight(1);
 
