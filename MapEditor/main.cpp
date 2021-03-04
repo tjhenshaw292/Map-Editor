a431413af4455c54f1d85285e42999d775cd3f49
TJ Henshaw <tjhenshaw292@hotmail.com>
2021-03-04 14:56:35 -0600

    added abyss

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,13 +5,13 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("gen1.png", sf::Vector2u(16, 16), 12, 8);
+	MapMaker makeMe("gen1.png", sf::Vector2u(16, 16), 150, 150);
 	makeMe.setBlankTile(551);
 
-	std::string fileName{ "houseOnBikePath.dat" };
+	std::string fileName{ "abyss.dat" };
 
 	makeMe.setMapFileName(fileName);
-	makeMe.load(fileName);
+	//makeMe.load(fileName);
 
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addRowToTop(1);
