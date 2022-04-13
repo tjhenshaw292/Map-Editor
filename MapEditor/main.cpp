bca56487c488033ed8ce789cc2f2e21b9a684fdb
TJ Henshaw <tjhenshaw292@hotmail.com>
2022-04-13 14:26:43 -0500

    Added Stadium Rooms 2-5 (stadium trainer fights)

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,18 +5,19 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("gen3indoors.png", sf::Vector2u(16, 16), 25, 25);
+	MapMaker makeMe("gen3indoors.png", sf::Vector2u(16, 16), 11, 13);
 	makeMe.setBlankTile(0); //551 for gen 1.....0 for gen 3
 
-	std::string fileName{ "stadiumRoom1.dat" };
+	std::string fileName{ "stadiumRoom5.dat" };
 
 	makeMe.setMapFileName(fileName);
-	makeMe.load(fileName);
+	//makeMe.load(fileName);
 
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addRowToTop(1);
 	//makeMe.addColumnToLeft(1);
 	//makeMe.addColumnToRight(1);
+	//makeMe.addRowToBottow(1);  192.144.96	200.160.112 144.152.152 160.160.160 136.96.64 160.128.80 232.232.184 200.200.160
 
 	makeMe.display();
 	makeMe.save(fileName);
