a9ec91e018586188488e214b30df9925cae444cd
TJ Henshaw <tjhenshaw292@hotmail.com>
2022-08-04 17:19:58 -0500

    Added Daniel House

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,10 +5,10 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("gen3Indoors.png", sf::Vector2u(16, 16), 18, 14);
+	MapMaker makeMe("gen3Indoors.png", sf::Vector2u(16, 16), 10, 7);
 	makeMe.setBlankTile(0); //551 for gen 1.....0 for gen 3
 
-	std::string fileName{ "regularHouse03.dat" };
+	std::string fileName{ "danielHouse.dat" };
 
 	makeMe.setMapFileName(fileName);
 	makeMe.load(fileName);
