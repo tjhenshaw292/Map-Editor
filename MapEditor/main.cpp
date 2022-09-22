504da637f9498a15977108818eaaaceb0206ff51
TJ Henshaw <tjhenshaw292@hotmail.com>
2022-09-22 15:35:28 -0500

    Dragons Cave

--- MapEditor/main.cpp
+++ MapEditor/main.cpp
@@ -5,10 +5,10 @@ int main()
 {
 	if (!MapMaker::mainFont.loadFromFile("Carlito-Regular.ttf"))
 		throw;
-	MapMaker makeMe("gen1.png", sf::Vector2u(16, 16), 32, 69);
-	makeMe.setBlankTile(551); //551 for gen 1.....0 for gen 3
+	MapMaker makeMe("gen3Indoors.png", sf::Vector2u(16, 16), 37, 32);
+	makeMe.setBlankTile(0); //551 for gen 1.....0 for gen 3
 
-	std::string fileName{ "supermaze.dat" };
+	std::string fileName{ "dragonsCave.dat" };
 
 	makeMe.setMapFileName(fileName);
 	makeMe.load(fileName);
@@ -16,8 +16,8 @@ int main()
 	//In order to use this, just add the row/col then reOpen mapmaker
 	//makeMe.addRowToTop(1);
 	//makeMe.addColumnToLeft(1);
-	//makeMe.addColumnToRight(1);
-	//makeMe.addRowToBottow(1);  192.144.96	200.160.112 144.152.152 160.160.160 136.96.64 160.128.80 232.232.184 200.200.160
+	//makeMe.addColumnToRight(5);
+	//makeMe.addRowToBottow(1);  232 x 163	171 x 145
 
 	makeMe.display();
 	makeMe.save(fileName);
